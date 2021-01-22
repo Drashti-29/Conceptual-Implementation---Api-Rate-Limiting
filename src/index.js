4259fa9f85ab3417c31b3a5623188d7f8a4d8b22
Drashti <72065999+Drashti-29@users.noreply.github.com>
2021-01-22 12:00:42 +0530

    appi

--- src/index.js
+++ src/index.js
@@ -1,19 +1,49 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const bodyParser = require("body-parser");
-const port = 3000
+const port = 3000;
 app.use(express.urlencoded());
-
+const Data = require("./initialData.js");
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(bodyParser.urlencoded({ extended: false }))
-
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 // your code goes here
 
+let numOfApiCalls = 0;
+let initialMax = null;
 
-app.listen(port, () => console.log(`App listening on port ${port}!`))
+app.get("/api/posts", (req, res) => {
+  if (numOfApiCalls >= 5) {
+    res.status(429).send({ message: "Exceed Number of API Calls" });
+    return;
+  }
+
+  const parsedMax = Number(req.query.max || 10);
+  const max = parsedMax > 20 ? 10 : parsedMax;
+  let finalMax = max;
+
+  if (initialMax !== null) {
+    finalMax = Math.min(finalMax, initialMax);
+  }
+
+  const topMax = posts.filter((value, idx) => idx < finalMax);
+  res.send(topMax);
+
+  if (initialMax === null) {
+    initialMax = max;
+    numOfApiCalls++;
+    setTimeout(() => {
+      initialMax = null;
+      numOfApiCalls = 0;
+    }, 30 * 1000);
+  } else {
+    numOfApiCalls++;
+  }
+});
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
 
 module.exports = app;
